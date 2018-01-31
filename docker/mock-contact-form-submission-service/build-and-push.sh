d8ef2c5ee8ee3d02451332d34a0e56cfe0e2e87c
James Hines <jameshines10@gmail.com>
2018-01-31 09:03:03 -0500

    Changes the image name to match service > worker > task

---
+++ docker/mock-contact-form-submission-service/build-and-push.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export VERSION=1.0.0
+
+docker build -t jhines2017/mock-contact-form-submission-worker:$VERSION . \
+&& docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
+&& docker push jhines2017/mock-form-submission-worker:$VERSION
