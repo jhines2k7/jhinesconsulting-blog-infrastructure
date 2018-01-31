d5aaa9cf99720d54df132e2bac1764d6c5426e77
James Hines <jameshines10@gmail.com>
2018-01-30 21:28:30 -0500

    Moves the call to use middlewares before the post request handler

--- docker/mock-form-submission-service/server.js
+++ docker/mock-form-submission-service/server.js
@@ -3,14 +3,15 @@ const server = jsonServer.create();
 const router = jsonServer.router();
 const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
 server.post('/contact', (req, res) => {
     res.status(200).jsonp({
         success: "The contact form was submitted successfully"
     })
 });
 
-server.use(middlewares);
 server.use(router);
 server.listen(3000, () => {
     console.log('Mock form submission service is running on port 3000')
