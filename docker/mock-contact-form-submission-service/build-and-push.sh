196018581158d4494742fcf2d63c36b743ba9728
James Hines <jameshines10@gmail.com>
2018-01-31 09:34:45 -0500

    Bumps version number

--- docker/mock-contact-form-submission-service/build-and-push.sh
+++ docker/mock-contact-form-submission-service/build-and-push.sh
@@ -4,4 +4,4 @@ export VERSION=1.0.0
 
 docker build -t jhines2017/mock-contact-form-submission-worker:$VERSION . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
-&& docker push jhines2017/mock-form-submission-worker:$VERSION
+&& docker push jhines2017/mock-contact-form-submission-worker:$VERSION
