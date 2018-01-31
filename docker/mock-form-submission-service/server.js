686eac06930c66ad19d747b95e1f30adb5f55b64
James Hines <jameshines10@gmail.com>
2018-01-30 19:28:46 -0500

    Setting up a mock service for form submission

---
+++ docker/mock-form-submission-service/server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router();
+const middlewares = jsonServer.defaults();
+
+server.post('/contact', (req, res) => {
+    res.status(200).jsonp({
+        success: "The contact form was submitted successfully"
+    })
+});
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+    console.log('Mock form submission service is running on port 3000')
+});
