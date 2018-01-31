104b1a585ddb9afa5c1435744e02c7198ba7e4fc
James Hines <jameshines10@gmail.com>
2018-01-31 11:10:01 -0500

    Adds a blog type node

--- scripts/remove-all-but-mysql-node.sh
+++ scripts/remove-all-but-mysql-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all but mysql node
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog');
     do docker-machine rm -f $machine; 
 done
--- scripts/remove-all-nodes.sh
+++ scripts/remove-all-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all nodes
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog');
     do docker-machine rm -f $machine;
 done
