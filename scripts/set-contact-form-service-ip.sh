7b86d1b7829990c6f56587bc1a6cf00db094da72
James Hines <jameshines10@gmail.com>
2018-02-04 09:30:28 -0500

    Changes function name

--- scripts/set-contact-form-service-ip.sh
+++ scripts/set-contact-form-service-ip.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-function get_machine_name {
+function get_contact_form_submission_service_machine_name {
     echo $(docker-machine ls --format "{{.Name}}" | grep 'mockcontactformsubmissionservice')
 }
 
-machine=$(get_manager_machine_name)
+machine=$(get_contact_form_submission_service_machine_name)
 
 export CONTACT_FORM_SUBMISSION_SERVICE_IP=$(docker-machine ip $machine):3000
 
 echo "Contact form submission service ip: "
 echo $CONTACT_FORM_SUBMISSION_SERVICE_IP
-
