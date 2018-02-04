599d4f34dc2deaa04b9cf90c5c907a1a36b1f9ac
James Hines <jameshines10@gmail.com>
2018-02-04 13:04:55 -0500

    Still trying to source env variables

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -104,6 +104,9 @@ function create_mock_contact_form_submission_service_node {
 
     source set-contact-form-service-ip.sh
 
+    echo "Contact form submission service ip: "
+    echo $CONTACT_FORM_SUBMISSION_SERVICE_IP
+
     bash /home/james/projects/jhines-consulting-blog/shell_scripts/build.sh
 }
 
