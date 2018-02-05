444437cdb991299884440081dc0ecb857822bcaa
James Hines <jameshines10@gmail.com>
2018-02-05 18:20:47 -0500

    Removes the directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,15 +17,15 @@ merged_compose_file="docker-stack.yml"
 
 echo "======> running compose config to create a merged compose file"
 docker-machine ssh $manager_machine sudo docker-compose \
-    -f /home/ubuntu/$kafka_compose_file \
-    -f /home/ubuntu/$http_source_compose_file config \
-    > /home/ubuntu/$merged_compose_file
+    -f $kafka_compose_file \
+    -f $http_source_compose_file config \
+    > $merged_compose_file
 
 echo "======> contents of the merged compose file"
-docker-machine ssh $manager_machine cat /home/ubuntu/$merged_compose_file
+docker-machine ssh $manager_machine cat $merged_compose_file
 
 docker-machine ssh $manager_machine \
     sudo docker stack deploy \
-    --compose-file /home/ubuntu/$merged_compose_file \
+    --compose-file $merged_compose_file \
     --with-registry-auth \
     blog
