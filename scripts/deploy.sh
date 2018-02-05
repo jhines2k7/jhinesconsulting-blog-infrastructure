3e9f9de19e6467b718237e046dbb77ff45e3d30b
James Hines <jameshines10@gmail.com>
2018-02-05 18:07:41 -0500

    Fixes a merged compose file issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,19 +13,19 @@ fi
 
 docker-machine ssh $manager_machine sudo docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD
 
-compose_file="docker-stack.yml"
+merged_compose_file="docker-stack.yml"
 
 echo "======> running compose config to create a merged compose file"
 docker-machine ssh $manager_machine sudo docker-compose \
-    -f $kafka_compose_file \
-    -f $http_source_compose_file config \
-    > /home/ubuntu/$compose_file
+    -f /home/ubuntu/$kafka_compose_file \
+    -f /home/ubuntu/$http_source_compose_file config \
+    > /home/ubuntu/$merged_compose_file
 
 echo "======> contents of the merged compose file"
-docker-machine ssh $manager_machine cat /home/ubuntu/$compose_file
+docker-machine ssh $manager_machine cat /home/ubuntu/$merged_compose_file
 
 docker-machine ssh $manager_machine \
     sudo docker stack deploy \
-    --compose-file /home/ubuntu/$compose_file \
+    --compose-file /home/ubuntu/$merged_compose_file \
     --with-registry-auth \
     blog
