f4ee9b3221c48b2d78d982e196f9da4648c2d5a1
James Hines <jameshines10@gmail.com>
2018-02-05 17:19:05 -0500

    Renames local variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 manager_machine=$(docker-machine ls --format "{{.Name}}" | grep 'manager')
 
-kafka_file="kafka-service.yml"
-http_source_file="http-source-task.yml"
+kafka_compose_file="kafka-service.yml"
+http_source_compose_file="http-source-task.yml"
 
 if [ "$ENV" = "dev" ]
 then
@@ -15,8 +15,8 @@ docker-machine ssh $manager_machine sudo docker login --username=$DOCKER_HUB_USE
 compose_file="docker-stack.yml"
 
 docker-machine ssh $manager_machine sudo docker-compose \
-    -f $kafka_file \
-    -f $http_source_file config \
+    -f $kafka_compose_file \
+    -f $http_source_compose_file config \
     > /home/ubuntu/$compose_file
 
 docker-machine ssh $manager_machine \
