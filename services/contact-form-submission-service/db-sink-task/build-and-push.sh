9f2d6861b25d88fcf0685b2efd60f3ba20d330b2
James Hines <jameshines10@gmail.com>
2018-02-06 12:21:57 -0500

    Log sink service looks good

--- services/contact-form-submission-service/db-sink-task/build-and-push.sh
+++ services/contact-form-submission-service/db-sink-task/build-and-push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# download the jar file and save to this directory
+wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/jdbc-sink-kafka-10/1.3.1.RELEASE/jdbc-sink-kafka-10-1.3.1.RELEASE-sources.jar
+
 docker build -t jhines2017/db-sink-worker:10-1.3.1.RELEASE . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
 && docker push jhines2017/db-sink-worker:10-1.3.1.RELEASE
