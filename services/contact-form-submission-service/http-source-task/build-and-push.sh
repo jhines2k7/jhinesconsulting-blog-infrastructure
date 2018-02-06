fe7c4fd7e630a272eff072a74b25a82cd4dac959
James Hines <jameshines10@gmail.com>
2018-02-06 12:28:09 -0500

    Updated env variables for mysql service

--- services/contact-form-submission-service/http-source-task/build-and-push.sh
+++ services/contact-form-submission-service/http-source-task/build-and-push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# download the jar file and save to this directory
+wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/http-source-kafka-10/1.3.1.RELEASE/http-source-kafka-10-1.3.1.RELEASE.jar
+
 docker build -t jhines2017/http-source-worker:10-1.3.1.RELEASE . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
 && docker push jhines2017/http-source-worker:10-1.3.1.RELEASE
