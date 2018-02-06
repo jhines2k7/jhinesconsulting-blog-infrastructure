8e999fa53194962740416efb533006bf400dad1c
James Hines <jameshines10@gmail.com>
2018-02-06 15:24:50 -0500

    Adds log sink and mysql service files to the list of compose files that will be merged

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -62,6 +62,7 @@ function merge_compose_files {
 
     local blog_ui_compose_file="../services/blog/blog.yml"
     local kafka_service_compose_file="../services/backing-services/kafka-service.yml"
+    local mysql_service_compose_file="../services/backing-services/mysql-service.yml"
     local log_sink_service_compose_file="../services/backing-services/log-sink-service/log-sink-service.yml"
     local http_source_task_compose_file="../services/contact-form-submission-service/http-source-task/http-source-task.yml"
     local db_sink_task_compose_file="../services/contact-form-submission-service/db-sink-task/db-sink-task.yml"
@@ -69,7 +70,9 @@ function merge_compose_files {
     echo "======> running docker compose to create a merged compose file"
     docker-compose \
     -f $db_sink_task_compose_file \
+    -f $log_sink_service_compose_file \
     -f $http_source_task_compose_file \
+    -f $mysql_service_compose_file \
     -f $kafka_service_compose_file config \
     > ../services/docker-stack.yml
 
