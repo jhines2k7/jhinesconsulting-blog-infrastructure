2a130247a732bcc95e22cc85a9f4249ea046bbc1
James Hines <jameshines10@gmail.com>
2018-02-06 13:18:08 -0500

    Adds informational logging to the scripts that build and push spring cloud spring app starter services

--- services/backing-services/log-sink-service/build-and-push.sh
+++ services/backing-services/log-sink-service/build-and-push.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/log-sink-kafka-10/1.3.1.RELEASE/log-sink-kafka-10-1.3.1.RELEASE.jar
+
+#check if jar file has already been downloaded
+FILE=log-sink-kafka-10-1.3.1.RELEASE.jar
+echo "=======> Checking for log sink jar file..."
+if [ -f $FILE ]; then
+   echo "=======> ...log sink jar file has already been downloaded"
+else
+    echo "=======> ...log sink jar file has not been downloaded"
+    echo "======> Downloading jar file for log sink service"
+    wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/log-sink-kafka-10/1.3.1.RELEASE/log-sink-kafka-10-1.3.1.RELEASE.jar
+fi
 
 docker build -t jhines2017/log-sink-service:10-1.3.1.RELEASE . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
--- services/contact-form-submission-service/http-source-task/build-and-push.sh
+++ services/contact-form-submission-service/http-source-task/build-and-push.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # download the jar file and save to this directory
-wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/http-source-kafka-10/1.3.1.RELEASE/http-source-kafka-10-1.3.1.RELEASE.jar
+FILE=http-source-kafka-10-1.3.1.RELEASE.jar
+echo "=======> Checking for http source jar file..."
+if [ -f $FILE ]; then
+   echo "=======> ...http source jar file has already been downloaded"
+else
+    echo "=======> ...http source jar file has not been downloaded"
+    echo "======> Downloading jar file for http source service"
+    wget http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/http-source-kafka-10/1.3.1.RELEASE/http-source-kafka-10-1.3.1.RELEASE.jar
+fi
 
 docker build -t jhines2017/http-source-worker:10-1.3.1.RELEASE . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
