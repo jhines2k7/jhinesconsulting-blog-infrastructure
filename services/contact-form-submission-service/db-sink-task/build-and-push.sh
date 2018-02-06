8473b13e27fc3288e987bc2b0ed0c2f220d778dd
James Hines <jameshines10@gmail.com>
2018-02-06 11:54:04 -0500

    Changes the output destination of the http source to match the input of the jdbc sink worker. Adds env variables to the db sink task configuration

--- services/contact-form-submission-service/db-sink-task/build-and-push.sh
+++ services/contact-form-submission-service/db-sink-task/build-and-push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t jhines2017/http-source-worker:10-1.3.1.RELEASE . \
+docker build -t jhines2017/db-sink-worker:10-1.3.1.RELEASE . \
 && docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD \
-&& docker push jhines2017/http-source-worker:10-1.3.1.RELEASE
+&& docker push jhines2017/db-sink-worker:10-1.3.1.RELEASE
