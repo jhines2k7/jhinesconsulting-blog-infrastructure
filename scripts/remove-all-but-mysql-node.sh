4a693745b38e74372e62262a275700176bd5f85d
James Hines <jameshines10@gmail.com>
2018-02-05 19:06:11 -0500

    Adds kafka

--- scripts/remove-all-but-mysql-node.sh
+++ scripts/remove-all-but-mysql-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all but mysql node
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog\|mock');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog\|mock\|kafka');
     do docker-machine rm -f $machine; 
 done
