01f46b164104c47f4033bfec271e40742a42a3e7
James Hines <jameshines10@gmail.com>
2018-02-13 14:33:29 -0500

    Comments out compose files for uneeded services

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ function merge_compose_files {
     local kafka_service_compose_file="../services/backing-services/kafka-service.yml"
     local mysql_service_compose_file="../services/backing-services/mysql-service.yml"
     local log_sink_service_compose_file="../services/backing-services/log-sink-service/log-sink-service.yml"
-    local contact_form_submission_request_task_compose_file="../services/contact-form-submission-service/contact-request-handler-task/contact-request-handler-task.yml"
+    local contact_request_task_compose_file="../services/contact-form-submission-service/contact-request-handler-task/contact-request-handler-task.yml"
     local db_sink_task_compose_file="../services/contact-form-submission-service/db-sink-task/db-sink-task.yml"
 
     if [ "$ENV" = "dev" ] ; then
@@ -15,10 +15,10 @@ function merge_compose_files {
     echo "======> running docker compose config to create a merged compose file"
 
     docker-compose \
-    -f $blog_ui_compose_file \
-    -f $db_sink_task_compose_file \
-    -f $log_sink_service_compose_file \
-    -f $contact_form_submission_request_task_compose_file \
+#    -f $blog_ui_compose_file \
+#    -f $db_sink_task_compose_file \
+#    -f $log_sink_service_compose_file \
+    -f $contact_request_task_compose_file \
     -f $mysql_service_compose_file \
     -f $kafka_service_compose_file config \
     > ../services/docker-stack.yml
