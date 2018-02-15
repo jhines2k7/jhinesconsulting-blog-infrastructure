84c7178d80b6c54eb19e6a3e1ac89b7fd726c0c7
James Hines <jameshines10@gmail.com>
2018-02-15 11:29:35 -0500

    Adds a column for client id

--- docker/db/jhinesconsulting-blog.sql
+++ docker/db/jhinesconsulting-blog.sql
@@ -6,6 +6,7 @@ USE jhinesconsulting;
 
 CREATE TABLE contact (
   id          INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  client_id   VARCHAR(255),
   name        VARCHAR(255),
   email       VARCHAR(255),
   message     LONGTEXT
