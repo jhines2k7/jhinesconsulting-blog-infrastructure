135b0f4f3f764fa1848d0955b93596850df15892
James Hines <jameshines10@gmail.com>
2018-02-15 11:27:51 -0500

    Comments uneeded services

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,15 +19,16 @@ function merge_compose_files {
     echo "======> running docker compose config to create a merged compose file"
 
 #    -f $blog_ui_compose_file \
+#    -f $save_contact_to_db_task \
     docker-compose \
     -f $contact_request_task_compose_file \
-    -f $save_contact_to_db_task \
     -f $log_sink_service_compose_file \
     -f $mysql_service_compose_file \
     -f $kafka_service_compose_file config \
     > ../services/docker-stack.yml
 
     echo "======> contents of the merged compose file"
+
     cat ../services/docker-stack.yml
 }
 
