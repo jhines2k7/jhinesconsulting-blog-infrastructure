3cfa1879d631a260f83bdddaedafe05364c17310
jhines2k7 <jameshines10@gmail.com>
2018-02-16 11:21:33 +0000

    Adds a column

--- docker/db/jhinesconsulting-blog.sql
+++ docker/db/jhinesconsulting-blog.sql
@@ -9,6 +9,7 @@ CREATE TABLE contact (
   client_id   VARCHAR(255),
   name        VARCHAR(255),
   email       VARCHAR(255),
-  message     LONGTEXT
+  message     LONGTEXT,
+  created     DATE
 )
 ENGINE = InnoDB;
