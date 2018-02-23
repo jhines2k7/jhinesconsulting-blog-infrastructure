7461ecde09258c2bc0cc41a1e26d5b514ab3239f
James Hines <jameshines10@gmail.com>
2018-02-23 12:05:15 -0500

    Switches the node to createprojectservice

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,9 +28,9 @@ function merge_compose_files {
 #    -f $blog_ui_compose_file \
 #    -f $save_contact_to_db_task_compose_file \
 #    -f $contact_request_task_compose_file \
-#    -f $log_sink_service_compose_file \
 #    -f $contacts_db_compose_file \
     docker-compose \
+    -f $log_sink_service_compose_file \
     -f $save_project_to_db_task_compose_file \
     -f $create_project_request_handler_task_compose_file \
     -f $list_projects_service_handler_task_compose_file \
