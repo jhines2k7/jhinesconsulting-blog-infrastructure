084bc45b6496c587c5bc9220e9ed684e26317312
James Hines <jameshines10@gmail.com>
2018-02-24 12:16:52 -0500

    Renames variables to make it easier to identify services and their tasks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,20 @@
 
 function merge_compose_files {
     local blog_ui_compose_file="../services/blog/blog-ui.yml"
-    local kafka_service_compose_file="../services/backing-services/kafka-service.yml"
-    local contacts_db_compose_file="../services/backing-services/contacts-db.yml"
-    local projects_db_compose_file="../services/backing-services/projects-db.yml"
-    local log_sink_service_compose_file="../services/backing-services/log-sink-service/log-sink-service.yml"
-    local contact_request_task_compose_file="../services/contact-service/contact-request-handler-task.yml"
-    local save_contact_to_db_task_compose_file="../services/contact-service/save-contact-to-db-task.yml"
-    local email_notification_task_compose_file="../services/contact-service/email-notification-task.yml"
-    local create_project_request_handler_task_compose_file="../services/create-project-service/create-project-task.yml"
-    local list_projects_service_handler_task_compose_file="../services/list-projects-service/list-projects-task.yml"
-    local save_project_to_db_task_compose_file="../services/create-project-service/save-project-to-db-task.yml"
+
+    local backing_services__kafka_service_compose_file="../services/backing-services/kafka-service.yml"
+    local backing_services__contacts_db_compose_file="../services/backing-services/contacts-db.yml"
+    local backing_services__projects_db_compose_file="../services/backing-services/projects-db.yml"
+    local backing_services__log_sink_service_compose_file="../services/backing-services/log-sink-service/log-sink-service.yml"
+
+    local contact_form_service__contact_form_handler_task_compose_file="../services/contact-form-service/contact-form-handler-task.yml"
+    local contact_form_service__save_contact_to_db_task_compose_file="../services/contact-form-service/save-contact-to-db-task.yml"
+    local contact_form_service__email_notification_task_compose_file="../services/contact-form-service/email-notification-task.yml"
+
+    local create_project_service__create_project_task_compose_file="../services/create-project-service/create-project-task.yml"
+    local create_project_service__save_project_to_db_task_compose_file="../services/create-project-service/save-project-to-db-task.yml"
+
+    local list_projects_service__list_projects_task_compose_file="../services/list-projects-service/list-projects-task.yml"
 
     if [ "$ENV" = "dev" ] ; then
         kafka_service_compose_file="../services/backing-services/kafka-service.dev.yml"
@@ -24,18 +28,18 @@ function merge_compose_files {
 
     echo "======> running docker compose config to create a merged compose file"
 
-#    -f $save_project_to_db_task_compose_file \
-#    -f $create_project_request_handler_task_compose_file \
-#    -f $list_projects_service_handler_task_compose_file \
+#    -f $create_project_service__save_project_to_db_task_compose_file \
+#    -f $create_project_service__create_project_task_compose_file \
+#    -f $list_projects_service__list_projects_task_compose_file \
 #    -f $projects_db_compose_file \
 #    -f $blog_ui_compose_file \
     docker-compose \
-    -f $save_contact_to_db_task_compose_file \
-    -f $contact_request_task_compose_file \
-    -f $contacts_db_compose_file \
-    -f $email_notification_task_compose_file \
-    -f $log_sink_service_compose_file \
-    -f $kafka_service_compose_file config \
+    -f $contact_form_service__save_contact_to_db_task_compose_file \
+    -f $contact_form_service__contact_form_handler_task_compose_file \
+    -f $contact_form_service__email_notification_task_compose_file \
+    -f $backing_services__contacts_db_compose_file \
+    -f $backing_services__log_sink_service_compose_file \
+    -f $backing_services__kafka_service_compose_file config \
     > ../services/docker-stack.yml
 
     echo "======> contents of the merged compose file"
@@ -54,9 +58,6 @@ function build_and_push_services {
     echo "======> LOGGING_INPUT_CHANNEL: $LOGGING_INPUT_CHANNEL"
     echo "======> Running build and push commands for spring cloud stream app starters"
     bash ../services/backing-services/log-sink-service/build-and-push.sh
-    bash ../services/create-project-service/save-project-to-db-task/build-and-push.sh &
-#    bash ../services/contact-form-submission-service/http-source-task/build-and-push.sh &
-    wait
 }
 
 build_and_push_services
