1cbb106494500e1654196dd7b6088322615b233c
James Hines <jameshines10@gmail.com>
2018-03-03 18:04:54 -0500

    Adds ui pattern

--- scripts/remove-all-nodes.sh
+++ scripts/remove-all-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all nodes
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog\|mock\|contact\|kafka\|db\|list\|create');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|512mb\|blog\|mock\|contact\|kafka\|projects\|list\|create\|ui');
     do docker-machine rm -f $machine;
 done
