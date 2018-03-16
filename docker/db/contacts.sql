5115a50bed8407df7966cf6d50ce632a7c9815d4
James Hines <jameshines10@gmail.com>
2018-03-16 17:09:21 -0400

    Bumps version number of save contact to db worker

--- docker/db/contacts.sql
+++ docker/db/contacts.sql
@@ -5,12 +5,12 @@ CREATE DATABASE IF NOT EXISTS jhinesconsulting_contacts;
 USE jhinesconsulting_contacts;
 
 CREATE TABLE contact (
-  id          INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  client_id   VARCHAR(255),
-  name        VARCHAR(255),
-  email       VARCHAR(255),
-  message     LONGTEXT,
-  created     DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  updated     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+  id             INT(11)   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  client_id      VARCHAR(255),
+  name           VARCHAR(255),
+  email          VARCHAR(255),
+  business_areas LONGTEXT,
+  created        DATETIME  NULL     DEFAULT CURRENT_TIMESTAMP,
+  updated        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
   ENGINE = InnoDB;
