ae6b986d5eaaa8887133de455f44c5cb5a314ddf
James Hines <jameshines10@gmail.com>
2018-05-01 14:19:57 -0400

    Adds a pattern for the contact service machine

--- scripts/remove-all-nodes.sh
+++ scripts/remove-all-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all nodes
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'jhcmanager\|jhc512mb\|jhcblog\|jhcmock\|jhccontactsdb\|jhckafka\|jhcprojects\|list\|jhccreate\|jhcui');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'jhcmanager\|jhc512mb\|jhcblog\|jhcmock\|jhccontactsdb\|jhckafka\|jhcprojects\|list\|jhccreate\|jhcui\|jhccontact');
     do docker-machine rm -f $machine;
 done
